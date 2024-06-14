d122e5d080aff02a1ddff84a58ad860a26d3222c
leticia <leticiaavolio09@gmail.com>
2024-06-13 22:26:09 -0300

    Alteração nas mensagens de Eroo

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var participantes = [];
 
 function addAmigos(){
+    const mensagemErro= document.querySelector('.mensagemErro');
     var nome = document.getElementById("nomeAmg").value.trim();
-
-    if(nome){
+ 
+    if (nome && !participantes.includes(nome)) {
         participantes.push(nome);
         document.getElementById("nomeAmg").value = "";
-        
-        document.getElementById("nomesAdd").value = participantes.join(", ")
+ 
+        document.getElementById("nomesAdd").value = participantes.join(", ");
+        mensagemErro.textContent=''
+    } else if (!nome) {
+        mensagemErro.textContent='ERRO - insira um nome'
+    } else if (participantes.includes(nome)) {
+        mensagemErro.textContent='ERRO - nome repetido'
     }
 }
+console.log(participantes)
